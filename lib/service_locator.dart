46cdf1cbaee5132e8376e1d99c1a6da009b7857c
ahed <ahedeid2000@gmail.com>
2023-08-27 14:09:41 +0300

    create locator

---
+++ lib/service_locator.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:gym_app/logic/localData/shared_pref.dart';
+import 'package:gym_app/routes/app_router.dart';
+import 'package:gym_app/utils/app_config.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+var sl = GetIt.instance;
+
+void init() async {
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  FirebaseFirestore firebaseFireStore = FirebaseFirestore.instance;
+  FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+
+  // ---------------------------------------------------------------------------
+  sl.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+  sl.registerLazySingleton<SharedPrefController>(
+      () => SharedPrefController(preferences: sl()));
+  sl.registerLazySingleton<AppConfig>(() => AppConfig());
+  sl.registerLazySingleton<AppRouter>(() => AppRouter());
+
+  // --------------------- Firebase --------------------------------------------
+  sl.registerLazySingleton<FirebaseAuth>(() => firebaseAuth);
+  sl.registerLazySingleton<FirebaseFirestore>(() => firebaseFireStore);
+  sl.registerLazySingleton<FirebaseStorage>(() => firebaseStorage);
+}
