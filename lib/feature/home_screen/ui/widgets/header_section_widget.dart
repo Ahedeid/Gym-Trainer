f64a5fe4559a49041072ca209ea6aa2c09fd9773
Yahya Abunada <zayns1914@gmail.com>
2023-08-28 21:57:46 +0200

    crate custom header

---
+++ lib/feature/home_screen/ui/widgets/header_section_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gym_app/utils/resources/colors_manger.dart';
+import 'package:gym_app/utils/resources/font_size.dart';
+
+class HeaderSectionWidget extends StatelessWidget {
+  final String? title;
+  final String? trailing;
+  final void Function()? onTap;
+  const HeaderSectionWidget({Key? key, this.title, this.trailing, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title!,
+          style: TextStyle(
+              color: ColorManager.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w400),
+        ),
+        GestureDetector(
+          onTap: onTap,
+          child: Text(
+            trailing!,
+            style: TextStyle(
+                color: ColorManager.black,
+                fontSize: FontSize.s12.sp,
+                fontWeight: FontWeight.w700),
+          ),
+        ),
+      ],
+    );
+  }
+}
