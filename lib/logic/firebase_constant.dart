1402043d4f011f0e7e4d27711523c0c01f3f1c6a
Yahya Abunada <zayns1914@gmail.com>
2023-08-28 21:57:07 +0200

    add variables

--- lib/logic/firebase_constant.dart
+++ lib/logic/firebase_constant.dart
@@ -2,8 +2,11 @@ class FirebaseConstant {
   FirebaseConstant._();
 
   static const usersCollection = 'users';
+  static const goalsCollection = 'goals';
+  static const categoriesCollection = 'exercise_categories';
   static const uid = 'uid';
   static const name = 'name';
   static const email = 'email';
   static const phone = 'phone';
+  static const goal = 'goal';
 }
