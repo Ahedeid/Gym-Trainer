1abf6809933dd8a03920ccac20ec6743f4a09515
Yahya Abunada <zayns1914@gmail.com>
2023-08-28 21:59:42 +0200

    fix & refactor

--- lib/feature/profile/provider/profile_provider.dart
+++ lib/feature/profile/provider/profile_provider.dart
@@ -1,16 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:gym_app/logic/localData/shared_pref.dart';
 import 'package:gym_app/routes/app_router.dart';
+import 'package:gym_app/routes/screen_name.dart';
 import 'package:gym_app/service_locator.dart';
 import 'package:gym_app/utils/helper.dart';
 
-
 class ProfileProvider extends ChangeNotifier {
-  String name = '';
-  String userName = '';
-  DocumentSnapshot? userData;
-
+  var auth = sl<FirebaseAuth>();
   //----------------------------------------------------------------------------
   bool isLoading = false;
 
@@ -19,28 +18,6 @@ class ProfileProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> getADataUser() async {
-    try {
-      setLoading(true);
-      String id = sl<SharedPrefController>().getUid();
-      // print(id);
-      final DocumentSnapshot userDoc =
-          await FirebaseFirestore.instance.collection('user').doc(id).get();
-      userData = userDoc;
-      name = userDoc.get('name');
-      userName = userDoc.get('userName');
-    } on FirebaseException catch (e) {
-      final message = e.message.toString();
-      UtilsConfig.showSnackBarMessage(
-        message: message,
-        status: false,
-      );
-    } finally {
-      setLoading(false);
-    }
-  }
-
-//------------------------------------------------------------------------------
   bool isLoadingEdit = false;
 
   setLoadingEdit(bool val) {
@@ -48,16 +25,15 @@ class ProfileProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> EditNameProfile({required String name,required String userName}) async {
+  Future<void> EditNameProfile(
+      {required String name, required String userName}) async {
     try {
       setLoadingEdit(true);
-      String id = sl<SharedPrefController>().getUid();
+      String id = sl<SharedPrefController>().getUserData().uid;
       await FirebaseFirestore.instance
           .collection('user')
           .doc(id)
-          .update({'name': name,
-      'userName': userName
-      });
+          .update({'name': name, 'userName': userName});
       sl<AppRouter>().back();
     } on FirebaseException catch (e) {
       final message = e.message.toString();
@@ -67,6 +43,27 @@ class ProfileProvider extends ChangeNotifier {
       );
     } finally {
       setLoadingEdit(false);
+    }
+  }
+
+  void _navigateToLoginPage() {
+    sl<AppRouter>().goToAndRemove(screenName: ScreenName.loginScreen);
+  }
+
+  Future<void> logout() async {
+    try {
+      // Clear navigation and any authentication-related data
+      _navigateToLoginPage(); // This should be a function that handles navigation properly
+      sl<SharedPrefController>().removeUser();
+
+      // Disconnect and sign out from Google if applicable
+      await sl<GoogleSignIn>().disconnect();
+      await sl<GoogleSignIn>().signOut();
+
+      // Sign out from Firebase Authentication
+      await auth.signOut();
+    } catch (e) {
+      print('Error signing out: $e');
     }
   }
 }
