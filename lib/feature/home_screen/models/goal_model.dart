28bbb7afe958821db18340fdd13b791db68f9992
Yahya Abunada <zayns1914@gmail.com>
2023-08-28 21:57:18 +0200

    create model

---
+++ lib/feature/home_screen/models/goal_model.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GoalModel {
+  final String name;
+  final String id;
+  final List<dynamic> categorieList;
+
+  GoalModel({
+    required this.name,
+    required this.id,
+    required this.categorieList,
+  });
+
+  factory GoalModel.fromDocumentSnapshot(DocumentSnapshot snapshot) {
+    final data = snapshot.data() as Map<String, dynamic>;
+    return GoalModel(
+      id: data["id"],
+      name: data['name'],
+      categorieList: data['categorie_list'],
+    );
+  }
+}
