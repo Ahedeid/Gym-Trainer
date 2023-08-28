ff794d01b5c697bd8f9be6c5c5de5a4cec1abca0
Yahya Abunada <zayns1914@gmail.com>
2023-08-28 21:58:43 +0200

    fix logic

--- lib/feature/registrations/provider/loginProvider.dart
+++ lib/feature/registrations/provider/loginProvider.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:gym_app/logic/firebase_constant.dart';
 import 'package:gym_app/logic/localData/shared_pref.dart';
 import 'package:gym_app/logic/model/user_model.dart';
 import 'package:gym_app/routes/app_router.dart';
@@ -27,31 +29,66 @@ class LoginProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  // Future<void> login({
+  //   required String email,
+  //   required String password,
+  // }) async {
+  //   if (loginFormKey.currentState!.validate()) {
+  //     try {
+  //       setLoading(true);
+  //       final credential = await fbAuth.signInWithEmailAndPassword(
+  //           email: email, password: password);
+  //       sl<SharedPrefController>().setLoggedIn();
+  //       final uid = credential.user!.uid;
+  //       print(uid);
+  //       sl<SharedPrefController>().saveUser(credential.user);
+  //       sl<SharedPrefController>().setUId(uid);
+  //       sl<ProfileProvider>().fetchUserData();
+  //
+  //       sl<AppRouter>()
+  //           .goToAndRemove(screenName: ScreenName.BNBUser, object: 0);
+  //       setLoading(false);
+  //     } on FirebaseException catch (e) {
+  //       setLoading(false);
+  //       final message = e.message.toString();
+  //       UtilsConfig.showSnackBarMessage(
+  //         message: message,
+  //         status: false,
+  //       );
+  //     }
+  //   }
+  // }
   Future<void> login({
     required String email,
     required String password,
   }) async {
-    if (loginFormKey.currentState!.validate()) {
-      try {
-        setLoading(true);
-        final credential = await fbAuth.signInWithEmailAndPassword(
-            email: email, password: password);
-        sl<SharedPrefController>().setLogedin();
-        final uid = credential.user!.uid;
-        print(uid);
-        sl<SharedPrefController>().setUId(uid);
-        // SharedPrefController().save(credential);
-        sl<AppRouter>()
-            .goToAndRemove(screenName: ScreenName.BNBUser, object: 0);
-        setLoading(false);
-      } on FirebaseException catch (e) {
-        setLoading(false);
-        final message = e.message.toString();
-        UtilsConfig.showSnackBarMessage(
-          message: message,
-          status: false,
-        );
-      }
+    try {
+      setLoading(true);
+      final credential = await fbAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Fetch user data from Firestore and update it
+      final userDoc = await sl<FirebaseFirestore>()
+          .collection(FirebaseConstant.usersCollection)
+          .doc(credential.user!.uid)
+          .get();
+
+      final userModel = UserModel.fromDocumentSnapshot(userDoc);
+
+      sl<SharedPrefController>().setLoggedIn();
+      sl<SharedPrefController>().saveUserData(userModel);
+
+      sl<AppRouter>().goToAndRemove(screenName: ScreenName.BNBUser, object: 0);
+    } on FirebaseException catch (e) {
+      final message = e.message.toString();
+      UtilsConfig.showSnackBarMessage(
+        message: message,
+        status: false,
+      );
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -71,6 +108,7 @@ class LoginProvider extends ChangeNotifier {
       try {
         setLoadingReSet(true);
         await fbAuth.sendPasswordResetEmail(email: email);
+
         sl<AppRouter>().goToAndRemove(screenName: ScreenName.checkEmailScreen);
         setLoadingReSet(false);
       } on FirebaseException catch (e) {
