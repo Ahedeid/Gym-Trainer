211f8ef342c0f3cc19b11e562bbe073e9c11b387
Yahya Abunada <zayns1914@gmail.com>
2023-09-04 14:34:19 +0200

    refactor

--- lib/feature/BNBUser.dart
+++ lib/feature/BNBUser.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:gym_app/feature/gym_family/ui/gym_family_screen.dart';
 import 'package:gym_app/feature/home_screen/ui/home_screen.dart';
-import 'package:gym_app/feature/our_shop/ui/our_shop_screen.dart';
+import 'package:gym_app/feature/meals_plan/ui/meals_plan_screen.dart';
 import 'package:gym_app/feature/profile/ui/profile_screen.dart';
 import 'package:gym_app/feature/workOut/ui/training_screen.dart';
-import 'package:gym_app/sheared/widget/CustomeSvg.dart';
+import 'package:gym_app/sheared/widget/CustomSvg.dart';
 import 'package:gym_app/utils/resources/colors_manger.dart';
 import 'package:gym_app/utils/resources/icons_constant.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
