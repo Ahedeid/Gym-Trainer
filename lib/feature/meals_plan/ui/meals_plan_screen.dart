593e2b3c3453b6d3968d72e1f52f3c2370a3ba2b
Yahya Abunada <zayns1914@gmail.com>
2023-09-05 14:40:32 +0200

    fix ui

--- lib/feature/meals_plan/ui/meals_plan_screen.dart
+++ lib/feature/meals_plan/ui/meals_plan_screen.dart
@@ -1,9 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:gym_app/feature/home_screen/ui/widgets/header_section_widget.dart';
+import 'package:gym_app/feature/meals_plan/models/meal_plan_model.dart';
+import 'package:gym_app/feature/meals_plan/ui/widgets/inspiration_card_list.dart';
+import 'package:gym_app/logic/firebase_constant.dart';
+import 'package:gym_app/service_locator.dart';
 import 'package:gym_app/sheared/widget/customAppBar.dart';
+import 'package:gym_app/utils/extensions/sized_box.dart';
+import 'package:gym_app/utils/resources/images_constant.dart';
 import 'package:gym_app/utils/resources/strings_in_app.dart';
 
-class OurShopScreen extends StatelessWidget {
-  const OurShopScreen({super.key});
+class MealsPlan extends StatelessWidget {
+  const MealsPlan({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +20,42 @@ class OurShopScreen extends StatelessWidget {
         title: AppStrings.mealsPlan,
         onTap: () {},
       ),
-      body: const Center(
-        child: Text(
-          'OurShop Screen',
-          style: TextStyle(
-              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              ClipRRect(
+                  borderRadius: BorderRadiusDirectional.circular(10),
+                  child: Image.asset(ImageApp.cover)),
+              20.addVerticalSpace,
+              HeaderSectionWidget(
+                title: AppStrings.dailyInspiration,
+                trailing: AppStrings.seeAll,
+              ),
+              10.addVerticalSpace,
+              StreamBuilder<QuerySnapshot>(
+                stream: sl<FirebaseFirestore>()
+                    .collection(FirebaseConstant.plans)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  final List plans = snapshot.data!.docs;
+
+                  return InspirationCardList(
+                      planList: plans
+                          .map((e) => MealPlan.fromDocumentSnapshot(e))
+                          .toList());
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
