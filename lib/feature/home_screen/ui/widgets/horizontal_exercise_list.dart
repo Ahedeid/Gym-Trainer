570b25d6c06d03386fd145c0767ec6df79e16a5c
ahed <ahedeid2000@gmail.com>
2023-09-09 16:26:07 +0300

    edit nav to exercises

--- lib/feature/home_screen/ui/widgets/horizontal_exercise_list.dart
+++ lib/feature/home_screen/ui/widgets/horizontal_exercise_list.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gym_app/feature/home_screen/models/exercise_model.dart';
+import 'package:gym_app/feature/home_screen/providers/home_provider.dart';
 import 'package:gym_app/feature/home_screen/ui/widgets/horizontal_exercise_widget.dart';
+import 'package:gym_app/routes/app_router.dart';
+import 'package:gym_app/routes/screen_name.dart';
+import 'package:gym_app/service_locator.dart';
+import 'package:gym_app/sheared/widget/bottomSheetDedailsWidget.dart';
 import 'package:gym_app/utils/extensions/sized_box.dart';
 
 class HorizontalExerciseList extends StatelessWidget {
@@ -26,8 +31,27 @@ class HorizontalExerciseList extends StatelessWidget {
         ],
       ),
       itemBuilder: (context, index) {
-        return HorizontalExerciseWidget(
-          exerciseModel: resultList[index],
+        bool? isLock = resultList[index].isLocked;
+        return GestureDetector(
+          onTap: (){
+            if (isLock == true) {
+              showModalBottomSheet(
+                  useSafeArea: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  context: context,
+                  builder: (context) => BottomSheetDetailsWidget());
+            } else {
+              sl<HomeProvider>()
+                  .getCategoryName(resultList[index].categoryId);
+              sl<AppRouter>().goTo(
+                  screenName: ScreenName.exercisesDetailsScreen,
+                  object: resultList[index]);
+            }
+          },
+          child: HorizontalExerciseWidget(
+            exerciseModel: resultList[index],
+          ),
         );
       },
     );
