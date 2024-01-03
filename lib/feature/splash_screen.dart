0172696186d2008b7de825ef8a379e3a43dff610
Yahya Abunada <zayns1914@gmail.com>
2024-01-03 15:29:58 +0100

    add animations

--- lib/feature/splash_screen.dart
+++ lib/feature/splash_screen.dart
@@ -9,56 +9,92 @@ import 'package:gym_app/utils/resources/images_constant.dart';
 import 'package:gym_app/utils/resources/strings_in_app.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _rotationAnimation;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3), // Adjust the duration as needed
+    );
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    _rotationAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    _animationController.forward();
     sl<AppConfig>().onBoardingStatue();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.primary,
-      body: Stack(
-        children: [
-          CustomSvgAssets(
-            path: AppIcons.vectorSplash,
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-            color: const Color(0xFF134179),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  ImageApp.logo,
-                  color: Colors.white,
-                  width: 200,
-                  height: 200,
-                ),
-                const SizedBox(height: 18),
-                Text(
-                  technoGym.tr(),
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: ColorManager.white),
-                )
-              ],
+      body: Center(
+        child: Stack(
+          children: [
+            CustomSvgAssets(
+              path: AppIcons.vectorSplash,
+              width: 10000,
+              height: 10000,
+              fit: BoxFit.fill,
+              color: const Color(0xFF134179),
             ),
-          ),
-        ],
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _scaleAnimation.value,
+                  child: Transform.rotate(
+                    angle: _rotationAnimation.value * 2 * 3.1416,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            ImageApp.logo,
+                            color: Colors.white,
+                            width: 250,
+                            height: 200,
+                          ),
+                          const SizedBox(height: 18),
+                          Text(
+                            technoGym.tr(),
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: ColorManager.white),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
